c6756ac29c7ea6b7fe5564d8d7c89933f8174851
tokku5552 <physics.0121@gmail.com>
2022-02-07 21:53:32 +0900

    ALBからの切り離しと再登録をデプロイ前後に行うよう修正

--- src/deploy.php
+++ src/deploy.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Deployer;
 
 require 'recipe/laravel.php';
@@ -7,7 +8,7 @@ require 'recipe/laravel.php';
 set('application', 'php-docker-nginx-postgresql');
 
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', false); 
+set('git_tty', false);
 
 // Shared files/dirs between deploys 
 add('shared_files', []);
@@ -16,13 +17,13 @@ add('shared_dirs', []);
 // Writable dirs by web server 
 add('writable_dirs', []);
 set('allow_anonymous_stats', false);
-    
+
 inventory('servers.yml');
 
 // Tasks
 
 task('build', function () {
-    run('cd {{release_path}} && build');
+  run('cd {{release_path}} && build');
 });
 
 // [Optional] if deploy fails automatically unlock.
@@ -33,7 +34,17 @@ after('deploy:failed', 'deploy:unlock');
 before('deploy:symlink', 'artisan:migrate');
 
 after('deploy:update_code', 'set_release_path');
-task('set_release_path',function() {
-    $newReleasePath = get('release_path') . '/src';
+task('set_release_path', function () {
+  $newReleasePath = get('release_path') . '/src';
   set('release_path', $newReleasePath);
+});
+
+before('deploy:info', 'deregister-targets');
+task('deregister-targets', function () {
+  runLocally('aws elbv2 deregister-targets --target-group-arn {{target_group_arn}} --targets Id={{instance_id}}');
+});
+
+after('deploy:unlock', 'register-targets');
+task('register-targets', function () {
+  runLocally('aws elbv2 register-targets --target-group-arn {{target_group_arn}} --targets Id={{instance_id}},Port=80 ');
 });
